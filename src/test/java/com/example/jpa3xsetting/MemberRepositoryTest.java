a765c48841203bbc65f4d19bd14b7d39ae6cdb11
kimminwoo6039 <qkqhemforhs1@gmail.com>
2023-01-17 20:17:56 +0900

    JPA 3.X 셋팅 PS6SY 로그

---
+++ src/test/java/com/example/jpa3xsetting/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.example.jpa3xsetting;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+
+@SpringBootTest
+
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test// 롤백을함.. 테스트
+    @Transactional
+    @Rollback(value = false)
+    public void testMember() throws Exception {
+
+
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+
+
+    }
+
+}
